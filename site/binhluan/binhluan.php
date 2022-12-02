63bbe93cc2c2b77845db3fcd1437ac5bfab8052a
hoangpv29 <hoangpv29@gmail.com>
2022-12-03 01:12:06 +0700

    hoang chinh duong link anh

--- site/binhluan/binhluan.php
+++ site/binhluan/binhluan.php
@@ -3,7 +3,6 @@ session_start();
 include "../../dao/pdo.php";
 include "../../dao/comment.php";
 
-
 if (isset($_SESSION['account']['id']) && $_SESSION['account']['id'] > 0) {
 
     // print_r($_SESSION);
@@ -14,7 +13,6 @@ if (isset($_SESSION['account']['id']) && $_SESSION['account']['id'] > 0) {
     }
     // if (isset($_GET["idsp"]) && ($_GET["idsp"] > 0)) {
     //     $id = $_GET["idsp"];
-
 
     // }
 
@@ -37,50 +35,104 @@ if (isset($_SESSION['account']['id']) && $_SESSION['account']['id'] > 0) {
     $dsbl = loadall_comment($_GET["idsp"]);
 
 ?>
-    <!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Document</title>
 </head>
-
+       
 <body>
-    <div class="boxcontent2 binhluan">
-        <table border="1">
-            <tr>
-                <th>Ten khach hang</th>
-                <th>Noi dung</th>
-                <th>Ngày bình luận</th>
+    <!-- binh luan -->
+    <?php
 
-            </tr>
-            <?php
-    foreach ($dsbl as $bl) {
-        extract($bl);
-        echo '<tr><td>' . $accountName . '</td>';
-        echo '<td>' . $content . '</td>';
-        echo '<td>' . $commentDate . '</td></tr>';
+    if (isset($_SESSION['account'])) {
+        extract($_SESSION['account']);
+       
+                $hinhpath = "../../uploads/" . $accountImage;
+                if (is_file($hinhpath)) {
+            // echo '<img src="'.$hinhpath.'" alt="" class="rounded-circle" style="width: 50px; height: 50px; bg-cover">
+            // ';
+            
+                  $accountImage = '<img src="' . $hinhpath . '" style="width: 65px; height: 50px; background-image: cover" class="rounded-circle">';
+                   
+                } else {
+                  $accountImage = '<img src="../../uploads/account.png" alt="" class="rounded-circle" style="width: 50px; height: 50px; bg-cover">';
+                }
+                
+                // if (isset($accountImage)) {
+
+                //   ">';
+                // } else {
+                //   echo '<img src="../uploads/trend-avatar-1.jpg" alt="" class="rounded-circle" style="width: 50px; height: 50px;">';
+                // }
+           
+           
+              
+        foreach ($dsbl as $bl) {
+            extract($bl);
+            echo '
+            <div class="d-flex row">
+            <div class="col-md-8">
+                <div class="d-flex flex-column comment-section">
+                    <div class="bg-white p-2">
+                        <div class="d-flex flex-row user-info">
+                            '.$accountImage.'
+                            <div class="d-flex flex-column justify-content-start ml-2"><span
+                                    class="d-block font-weight-bold name">'.$accountName.'</span><span
+                                    class="date text-black-50">'.$commentDate.'</span></div>
+                        </div>
+                        <div class="mt-2">
+                            <p class="comment-text">'.$content.'</p>
+                        </div>
+                    </div>
+    
+                </div>
+            </div>
+        </div>
+            
+            
+            ';
+        }
     }
-            ?>
-        </table>
-    </div>
-    <form action="binhluan.php" method="post">
+    
+    ?>
+   
+    
+     <form action="binhluan.php" method="post" class="d-flex">
 
-        <input type="text" name="content" required class="noidung">
-        <input type="hidden" name="productId" value="<?= $_GET['idsp'] ?>">
-        <input type="hidden" name="accountId" id="" value="<?= $accountId ?>">
-        <input type="hidden" name="accountName" id="" value="<?= $accountName ?>">
-        <input type="hidden" name="commentDate" value="<?= $commentDate ?>">
-        <input type="submit" name="guibl" class="guibinhluan" value="Gui">
-    </form>
+       
 
-    </body>
 
-    </html>
+</div>
+<?=$accountImage?>
+<!-- <img src="../../uploads/gucci1.jpg" alt=""> -->
+<textarea class="form-control ml-1 shadow-none textarea" name="content" required ></textarea>
+<!-- <input type="text" name="content" required class="noidung"> -->
+<input type="hidden" name="productId" value="<?= $_GET['idsp'] ?>">
+<input type="hidden" name="accountId" id="" value="<?= $accountId ?>">
+<input type="hidden" name="accountName" id="" value="<?= $accountName ?>">
+<input type="hidden" name="commentDate" value="<?= $commentDate ?>">
+<input type="submit" name="guibl" class="guibinhluan btn btn-danger" value="Gui">
+</form>
+    <!-- end bl -->
+  
+
+    
+
+</body>
+
+</html>
 <?php } else {
     $link = 'http://localhost/du-an-1/index.php?act=dangnhap';
     echo "<a href='$link' target='_parent'>Vui long dang nhap</a>";
 } ?>
 <?php $dsbl = loadall_comment($_GET["idsp"]); ?>
+
