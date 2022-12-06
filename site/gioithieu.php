99b1e803cd7e36a0d15ae061adbd2eb8164a48aa
hoangpv29 <hoangpv29@gmail.com>
2022-12-07 00:36:17 +0700

    hoang them gt tt

---
+++ site/gioithieu.php
@@ -0,0 +1,80 @@
+</div>
+
+    <div class="container text-center w-75 p-4">
+        <div class="title">
+            <h2>CHUẨN NƯỚC HOA</h2>
+        </div>
+
+        <div class="row row-main m-5">
+            <div class="content-lage d-flex ">
+                <div class="content-left col-6 text-justify">
+                    <div class="title-item">
+                        <h2>Vì Sao Bạn Nên TIN & CHỌN Chuẩn Perfume</h2>
+                    </div>
+                    <div class="item-1">
+                        <p>Chuẩn Perfume mong muốn đem đến cho khách hàng những sản phẩm có giá trị sử dụng tuyệt vời
+                            👍.
+                        </p>
+                    </div>
+                    <div class="item-1">
+                        <p>Chuẩn Authentic tự tin là một trong những nơi kinh doanh nước hoa, chính hãng, có uy tín trên
+                            thị
+                            trường.</p>
+                    </div>
+                    <div class="item-1">Bằng những nỗ lực không ngừng nghỉ trong việc kinh doanh online và offline,
+                        Chuẩn
+                        Authentic đã trao gởi hàng ngàn sản phẩm tới tay khách hàng cả nước và nhận được hàng ngàn lời
+                        khen
+                        về chất lượng sản phẩm.</div>
+                </div>
+                <div class="content-right col-6">
+                    <iframe title="Gốc Nước Hoa - Chuẩn Perfume" width="440" height="355"
+                        src="https://www.youtube.com/embed/YHYLX7TOeTQ?feature=oembed" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+        <div class="row m-5">
+            <div class="content-lage d-flex ">
+                <div class="content-right col-6">
+                    <iframe title="Gốc Nước Hoa - Chuẩn Perfume" width="450" height="355"
+                        src="https://www.youtube.com/embed/YHYLX7TOeTQ?feature=oembed" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                </div>
+                <div class="content-left col-6 text-justify">
+                    <div class="title-item">
+                        <h2>Vì Sao Bạn Nên TIN & CHỌN Chuẩn Perfume</h2>
+                    </div>
+                    <div class="item-1">
+                        <p>Chuẩn Perfume mong muốn đem đến cho khách hàng những sản phẩm có giá trị sử dụng tuyệt vời
+                            👍.
+                        </p>
+                    </div>
+                    <div class="item-1">
+                        <p>Chuẩn Authentic tự tin là một trong những nơi kinh doanh nước hoa, chính hãng, có uy tín trên
+                            thị
+                            trường.</p>
+                    </div>
+                    <div class="item-1">Bằng những nỗ lực không ngừng nghỉ trong việc kinh doanh online và offline,
+                        Chuẩn
+                        Authentic đã trao gởi hàng ngàn sản phẩm tới tay khách hàng cả nước và nhận được hàng ngàn lời
+                        khen
+                        về chất lượng sản phẩm.</div>
+                </div>
+
+            </div>
+        </div>
+        <div class="row ">
+            <p class="text-center col-12">Đến với Chuẩn Perfume, bạn sẽ hoàn toàn yên tâm về chất lượng sản phẩm, giá thành hợp lý, tư vấn nhiệt tình, có hiểu biết, giao hàng nhanh chóng và nhiều chương trình khuyến mãi, giảm giá hấp dẫn. Với hàng ngàn sản phẩm nước hoa, Chuẩn Authentic sẽ đáp ứng đa dạng nhu cầu thời trang của khách hàng.</p>
+        </div>
+        <div class="row my-4">
+            <h3 >Chuẩn Perfume Nâng Tầm Cuộc Sống</h2>
+        </div>
+
+
+    </div>
+
+
+</body>
---
+++ site/tintuc.php
@@ -0,0 +1,150 @@
+</div>
+
+<div class="container text-center w-75 p-4">
+    <div class="title">
+        <h2>CHUẨN NƯỚC HOA</h2>
+    </div>
+
+
+    <div class="row m-5">
+        <div class="content-lage d-flex ">
+            <div class="content-right col-6">
+                <img class="w-75" src="7.jpgchanel1.jpg" alt="Anh bi loi">
+            </div>
+            <div class="content-left col-6 text-justify">
+                <div class="title-item">
+                    <h2>Vì Sao Bạn Nên TIN & CHỌN Chuẩn Perfume</h2>
+                </div>
+             
+                <div class="item-1">
+                    <p>Chuẩn Authentic tự tin là một trong những nơi kinh doanh nước hoa, chính hãng, có uy tín trên
+                        thị
+                        trường.</p>
+                </div>
+                <div class="item-1">Bằng những nỗ lực không ngừng nghỉ trong việc kinh doanh online và offline,
+                    Chuẩn
+                    Authentic đã trao gởi hàng ngàn sản phẩm tới tay khách hàng cả nước và nhận được hàng ngàn lời
+                    khen...       <button class="btn btn-danger my-3">Read more...</button>
+                </div>
+                <div>
+             
+
+                </div>
+            </div>
+            
+
+        </div>
+    </div>
+
+
+    <div class="row m-5">
+        <div class="content-lage d-flex ">
+            <div class="content-right col-6">
+                <img class="w-75" src="7.jpggucci3.jpg" alt="Anh bi loi">
+            </div>
+            <div class="content-left col-6 text-justify">
+                <div class="title-item">
+                    <h2>Hướng Dẫn Bảo Quản Nước Hoa Đúng Mà Cách Ai Cũng Làm Được</h2>
+                </div>
+             
+                <div class="item-1">
+                    <p>Chuẩn Authentic tự tin là một trong những nơi kinh doanh nước hoa, chính hãng, có uy tín trên
+                        thị
+                        trường.</p>
+                </div>
+                <div class="item-1">Bằng những nỗ lực không ngừng nghỉ trong việc kinh doanh online và offline,
+                    Chuẩn
+                    Authentic đã trao gởi hàng ngàn sản phẩm tới tay khách hàng cả nước và nhận được hàng ngàn lời
+                    khen... <button class="btn btn-danger my-3">Read more...</button>
+                </div>
+                <div>
+                   
+
+                </div>
+            </div>
+            
+
+        </div>
+    </div>
+
+
+    <div class="row m-5">
+        <div class="content-lage d-flex ">
+            <div class="content-right col-6">
+                <img class="w-75" src="../site/src/img/nuochoa.jpg" alt="Anh bi loi">
+            </div>
+            <div class="content-left col-6 text-justify">
+                <div class="title-item">
+                    <h2>Vì Sao Bạn Nên TIN & CHỌN Chuẩn Perfume</h2>
+                </div>
+             
+                <div class="item-1">
+                    <p>Chuẩn Authentic tự tin là một trong những nơi kinh doanh nước hoa, chính hãng, có uy tín trên
+                        thị
+                        trường.</p>
+                </div>
+                <div class="item-1">Bằng những nỗ lực không ngừng nghỉ trong việc kinh doanh online và offline,
+                    Chuẩn
+                    Authentic đã trao gởi hàng ngàn sản phẩm tới tay khách hàng cả nước và nhận được hàng ngàn lời
+                    khen... <button class="btn btn-danger my-3">Read more...</button>
+                </div>
+                <div>
+                   
+
+                </div>
+            </div>
+            
+
+        </div>
+    </div>
+
+
+
+    <div class="row m-5">
+        <div class="content-lage d-flex ">
+            <div class="content-right col-6">
+                <img class="w-75" src="../site/src/img/nuochoa.jpg" alt="Anh bi loi">
+            </div>
+            <div class="content-left col-6 text-justify">
+                <div class="title-item">
+                    <h2>Vì Sao Bạn Nên TIN & CHỌN Chuẩn Perfume</h2>
+                </div>
+             
+                <div class="item-1">
+                    <p>Chuẩn Authentic tự tin là một trong những nơi kinh doanh nước hoa, chính hãng, có uy tín trên
+                        thị
+                        trường.</p>
+                </div>
+                <div class="item-1">Bằng những nỗ lực không ngừng nghỉ trong việc kinh doanh online và offline,
+                    Chuẩn
+                    Authentic đã trao gởi hàng ngàn sản phẩm tới tay khách hàng cả nước và nhận được hàng ngàn lời
+                    khen...  <button class="btn btn-danger my-3">Read more...</button>
+                </div>
+                <div>
+                  
+
+                </div>
+            </div>
+            
+
+        </div>
+    </div>
+    <div class="row ">
+        <p class="text-center col-12">Đến với Chuẩn Perfume, bạn sẽ hoàn toàn yên tâm về chất lượng sản phẩm, giá
+            thành hợp lý, tư vấn nhiệt tình, có hiểu biết, giao hàng nhanh chóng và nhiều chương trình khuyến mãi,
+            giảm giá hấp dẫn. Với hàng ngàn sản phẩm nước hoa, Chuẩn Authentic sẽ đáp ứng đa dạng nhu cầu thời trang
+            của khách hàng.</p>
+    </div>
+    <div class="row my-4">
+        <h3>Chuẩn Perfume Nâng Tầm Cuộc Sống</h2>
+    </div>
+
+
+</div>
+
+<style>
+    .item-1,.title-item{
+        text-align: justify;
+    }
+</style>
+</body>
